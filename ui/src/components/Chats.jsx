e1a862cb1baa8a80a1710c29c674cd03543f7321
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-15 02:45:33 +0700

    feat: chats area

---
+++ ui/src/components/Chats.jsx
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from "react";
+import Box from "@mui/material/Box";
+import ChatBubble from "./ChatBubble";
+
+function Chats({messages}) {
+  const chatsRef = useRef(null);
+
+  useEffect(() => {
+    if (chatsRef.current) {
+      chatsRef.current.scrollTo({
+        top: chatsRef.current.scrollHeight,
+        behavior: "smooth", // Smooth scrolling
+      });
+    }
+  }, [messages]);
+
+  return (
+    <Box
+      ref={chatsRef}
+      sx={{
+        flexGrow: 1,
+        width: "100%",
+        margin: "20px",
+        maxWidth: "600px",
+        display: "flex",
+        flexDirection: "column-reverse", // Start from bottom
+        overflowY: "auto",
+        boxSizing: "border-box",
+      }}
+    >
+      {messages
+        .slice(0)
+        .reverse()
+        .map((message, index) => (
+          <ChatBubble
+            key={index}
+            speaker={message.speaker}
+            message={message.message}
+          />
+        ))}
+    </Box>
+  );
+}
+
+export default Chats;
