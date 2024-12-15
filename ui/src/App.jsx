88f781ae9b76a88c51a3654c65beb6bf858c1c3f
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-15 21:30:50 +0700

    feat: fetch bot response

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,12 +4,38 @@ import ChatInput from "./components/ChatInput";
 import Chats from "./components/Chats";
 
 function App() {
+  const BOT_HOST = import.meta.env.VITE_BOT_HOST;
+  const BOT_PORT = import.meta.env.VITE_BOT_PORT;
+  const BOT_URL = `http://${BOT_HOST}:${BOT_PORT}/chat`;
+
   const [messages, setMessages] = useState([
     { speaker: "bot", message: "Hello! How can I assist you today?" },
   ]);
 
+  const getBotResponse = async (userMessage) => {
+    const response = await fetch(BOT_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: userMessage})
+    });
+
+    if (!response.ok) {
+      console.error("Failed to get response from bot");
+      return;
+    }
+
+    const data = await response.json();
+    addMessage({ speaker: "bot", message: data.response });
+  }
+
   const addMessage = (newMessage) => {
     setMessages((prev) => [...prev, newMessage]);
+    
+    if (newMessage.speaker === "user") {
+      getBotResponse(newMessage.message);
+    }
   };
 
   return (
